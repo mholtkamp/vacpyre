bdb8788b5042ff6179a5bcab1aab3f973ea43e4e
mholtkamp <mholt012@gmail.com>
2026-01-28 13:27:59 -0500

    Add button hints

--- Vacpyre/Scripts/InGameMenu.lua
+++ Vacpyre/Scripts/InGameMenu.lua
@@ -2,7 +2,6 @@ InGameMenu = {}
 
 function InGameMenu:Create()
 
-
 end
 
 function InGameMenu:GatherProperties()
@@ -12,6 +11,7 @@ function InGameMenu:GatherProperties()
         { name = "resume", type = DatumType.Node},
         { name = "mainMenu", type = DatumType.Node},
         { name = "quit", type = DatumType.Node},
+        { name = "hintText", type = DatumType.Node},
     }
 end
 
@@ -20,6 +20,30 @@ function InGameMenu:Start()
     self.resume:ConnectSignal("Activated", self, InGameMenu.Close)
     self.mainMenu:ConnectSignal("Activated", self, InGameMenu.GotoMainMenu)
     self.quit:ConnectSignal("Activated", self, InGameMenu.QuitGame)
+
+    local useGamepad = Input.IsGamepadConnected(1)
+
+    local hintStr = ""
+
+    if (useGamepad) then
+        hintStr = hintStr .. "`f33`Move:`fff` Right Stick\n"
+        hintStr = hintStr .. "`f33`Look:`fff` Left Stick\n"
+        hintStr = hintStr .. "`f33`Suck Object:`fff` Hold L\n"
+        hintStr = hintStr .. "`f33`Shoot Object:`fff` Release L\n"
+        hintStr = hintStr .. "`f33`Jump:`fff` A\n"
+        hintStr = hintStr .. "`f33`Drop Object:`fff` B\n"
+        hintStr = hintStr .. "`f33`Reload Last Checkpoint:`fff` Y\n"
+    else
+        hintStr = hintStr .. "`f33`Move:`fff` W/A/S/D\n"
+        hintStr = hintStr .. "`f33`Look:`fff` Move Mouse\n"
+        hintStr = hintStr .. "`f33`Suck Object:`fff` Hold Left Click\n"
+        hintStr = hintStr .. "`f33`Shoot Object:`fff` Release Left Click\n"
+        hintStr = hintStr .. "`f33`Jump:`fff` Space\n"
+        hintStr = hintStr .. "`f33`Drop Object:`fff` E\n"
+        hintStr = hintStr .. "`f33`Reload Last Checkpoint:`fff` R\n"
+    end
+
+    self.hintText:SetText(hintStr)
 
 end
 
