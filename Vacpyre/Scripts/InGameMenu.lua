41d818bc07fa25d91ad8253ff4ae7d6ecf3e5d93
mholtkamp <mholt012@gmail.com>
2026-01-31 21:51:40 -0500

    Fix ingame menu on 3ds

--- Vacpyre/Scripts/InGameMenu.lua
+++ Vacpyre/Scripts/InGameMenu.lua
@@ -19,9 +19,16 @@ end
 
 function InGameMenu:Start()
 
+    if (self.started) then
+        return
+    end
+
     self.resume:ConnectSignal("Activated", self, InGameMenu.Close)
     self.mainMenu:ConnectSignal("Activated", self, InGameMenu.GotoMainMenu)
     self.quit:ConnectSignal("Activated", self, InGameMenu.QuitGame)
+
+    self.options = self:FindChild("Options", true)
+
 
     local useGamepad = Input.IsGamepadConnected(1)
 
@@ -47,6 +54,8 @@ function InGameMenu:Start()
 
     self.hintText:SetText(hintStr)
 
+    self.started = true
+
 end
 
 function InGameMenu:Tick(detlaTime)
@@ -63,6 +72,10 @@ end
 
 function InGameMenu:Open(controller)
 
+    if (not self.started) then
+        self:Start()
+    end
+
     self.resume:SetActive(true)
 
     Button.SetSelected(self.resume)
@@ -74,6 +87,17 @@ function InGameMenu:Open(controller)
     Input.LockCursor(false)
     Input.TrapCursor(false)
     Input.ShowCursor(true)
+
+    if (Engine.GetPlatform() == "3DS") then
+        local world2 = Engine.GetWorld(2)
+        if (world2:GetRootNode() == nil) then
+        else
+            local root2 = world2:GetRootNode()
+        end
+        world2:GetRootNode():AddChild(self.options)
+        self.options:SetAnchorMode(AnchorMode.FullStretch)
+        self.options:SetRatios(0.1, 0.1, 0.8, 0.8)
+    end
 
     self.timeOpen = 0.0
 
@@ -88,6 +112,10 @@ function InGameMenu:Close()
     self.controller.enableControl = true
     self:Detach()
 
+    if (Engine.GetPlatform() == "3DS") then
+        self.options:Detach()
+    end
+
     Input.LockCursor(true)
     Input.TrapCursor(true)
     Input.ShowCursor(false)
@@ -98,6 +126,10 @@ function InGameMenu:GotoMainMenu()
 
     if (self.timeOpen < 0.1) then
         return
+    end
+
+    if (Engine.GetPlatform() == "3DS") then
+        Engine.GetWorld(2):Clear()
     end
 
     self.world:LoadScene("SC_MainMenu")
